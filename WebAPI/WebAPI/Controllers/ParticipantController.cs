69fd78cb89ef59935e06a40a614018bd5add85fc
Dhanika Munasinghe <dtm@tiqri.com>
2018-03-26 17:47:22 +0530

    First Commit

---
+++ WebAPI/WebAPI/Controllers/ParticipantController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ParticipantController : ApiController
+    {
+        [HttpPost]
+        [Route("api/InsertParticipant")]
+        public Participant Insert(Participant model)
+        {
+            using (DBModels db = new DBModels())
+            {
+                db.Participants.Add(model);
+                db.SaveChanges();
+                return model;
+            }
+
+        }
+
+        [HttpPost]
+        [Route("api/UpdateOutPut")]
+        public void UpdateOutPut(Participant model)
+        {
+            using (DBModels db = new DBModels())
+            {
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
